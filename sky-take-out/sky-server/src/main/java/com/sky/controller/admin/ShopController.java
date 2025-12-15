92269bf31a015f10ebaead37e225599b664a3006
Sir1527 <pengcheng.tang@outlook.com>
2025-12-15 14:38:53 +0800

    更换数据源

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/ShopController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -6,9 +6,10 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("userShopController")
+@RestController("adminShopController")
 @RequestMapping("/admin/shop")
 @Api(tags = "店铺操作相关接口")
 @Slf4j
